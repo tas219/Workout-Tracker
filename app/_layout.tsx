5d9b966428506a8e2488dbd3bf98c0f8321adf90
Tasya <122555509+tas219@users.noreply.github.com>
2025-05-25 18:46:00 -0400

    Added fonts, not loading in expo go

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,16 @@
 import { Text, View } from 'react-native'
 import { Stack } from 'expo-router'
 import React from 'react'
+import { useFonts } from "expo-font";
 
 const RootLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+  });
   return( 
     <Stack>
       <Stack.Screen name='index'/>
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,17 @@
 import { Text, View } from "react-native";
 import "../global.css"
 
+
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-3xl bg-quinary-light dark:bg-quinary-dark text-primaryText-light dark:text-primaryText-dark" >I changed something.</Text>
+    <View className="flex-1 items-center justify-center bg-quinary-light dark:bg-quinary-dark ">
+      <Text className="text-3xl font-pbold text-primaryText-light dark:text-primaryText-dark" >
+        Big Title
+        </Text>
+        <Text className="font-pregular">
+          More text for checking out the fonts and why isnt it working
+        </Text>
+        {/* <p className="font-pextrabold">some text to make sure the fonts are working</p> */}
     </View>
   );
 }
