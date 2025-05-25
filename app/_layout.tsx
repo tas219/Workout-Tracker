d3d79d1191284aca24001b7f250ca8e322bd7ac5
Tasya <122555509+tas219@users.noreply.github.com>
2025-05-25 19:18:04 -0400

    Fixed dependencies and fonts or something

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
-import { Text, View } from 'react-native'
-import { Stack } from 'expo-router'
-import React from 'react'
+//import React from 'react'
 import { useFonts } from "expo-font";
+import { useEffect } from "react";
+//import "react-native-url-polyfill/auto";
+import { SplashScreen, Stack } from "expo-router";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
   const [fontsLoaded, error] = useFonts({
@@ -11,6 +14,23 @@ const RootLayout = () => {
     "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
     "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
   });
+
+   useEffect(() => {
+    if (error) throw error;
+
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
   return( 
     <Stack>
       <Stack.Screen name='index'/>
